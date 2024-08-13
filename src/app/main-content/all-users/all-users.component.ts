5d6b06c73ee503b4dbf4591ad401e2ac46ef43df
MahmoudMoheyEldeen <mahmoudmohey1992@gmail.com>
2024-08-13 14:48:27 +0300

    test ang mat

--- src/app/main-content/all-users/all-users.component.ts
+++ src/app/main-content/all-users/all-users.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-all-users',
   templateUrl: './all-users.component.html',
-  styleUrls: ['./all-users.component.scss']
+  styleUrls: ['./all-users.component.scss'],
 })
 export class AllUsersComponent {
+  hidden = false;
 
+  toggleBadgeVisibility() {
+    this.hidden = !this.hidden;
+  }
 }
--- src/app/main-content/main-content.module.ts
+++ src/app/main-content/main-content.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
 import { MainContentRoutingModule } from './main-content-routing.module';
 import { AllUsersComponent } from './all-users/all-users.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
@@ -8,6 +10,13 @@ import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AllUsersComponent, UserDetailsComponent],
-  imports: [CommonModule, MainContentRoutingModule, RouterModule],
+  imports: [
+    CommonModule,
+    MainContentRoutingModule,
+    RouterModule,
+    MatBadgeModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
 export class MainContentModule {}
