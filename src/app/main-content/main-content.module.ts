d10a9ab62dc8535a5e4a527d726cd9e14dc9813f
MahmoudMoheyEldeen <mahmoudmohey1992@gmail.com>
2024-08-13 14:53:57 +0300

    added angular material

--- src/app/main-content/main-content.module.ts
+++ src/app/main-content/main-content.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatBadgeModule } from '@angular/material/badge';
 import { MainContentRoutingModule } from './main-content-routing.module';
 import { AllUsersComponent } from './all-users/all-users.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
@@ -10,13 +7,6 @@ import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AllUsersComponent, UserDetailsComponent],
-  imports: [
-    CommonModule,
-    MainContentRoutingModule,
-    RouterModule,
-    MatBadgeModule,
-    MatButtonModule,
-    MatIconModule,
-  ],
+  imports: [CommonModule, MainContentRoutingModule, RouterModule],
 })
 export class MainContentModule {}
