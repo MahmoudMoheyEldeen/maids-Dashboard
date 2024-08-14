cefc69482f0f2c6328cec9077fbf68486c44db4f
MahmoudMoheyEldeen <mahmoudmohey1992@gmail.com>
2024-08-14 16:39:53 +0300

    get table pagination

--- src/app/main-content/all-users/all-users.component.ts
+++ src/app/main-content/all-users/all-users.component.ts
@@ -11,6 +11,10 @@ import { SearchService } from 'src/app/services/search.service';
 export class AllUsersComponent implements OnInit {
   users: User[] = [];
   searchId: number = 0;
+  page: number = 0;
+  per_page: number = 0;
+  total: number = 0;
+  total_pages: number = 0;
 
   constructor(
     private _allUserServices: UserServiceService,
@@ -28,6 +32,10 @@ export class AllUsersComponent implements OnInit {
     this._allUserServices.getAllUsers().subscribe({
       next: (resp) => {
         this.users = resp.data;
+        this.per_page = resp.per_page;
+        this.total = resp.total;
+        this.total_pages = resp.total_pages;
+        this.page = resp.page;
       },
       error: (err) => {
         console.log(err);
