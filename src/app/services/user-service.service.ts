a5489e999c404de840c48b7d1e15afe868c4be4b
MahmoudMoheyEldeen <mahmoudmohey1992@gmail.com>
2024-08-14 19:22:23 +0300

    chached data returned from api

--- src/app/services/user-service.service.ts
+++ src/app/services/user-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, shareReplay, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,12 +9,36 @@ export class UserServiceService {
   apiBaseLink = 'https://reqres.in/api/users?page={page}';
   userDetailsBaseUrl = 'https://reqres.in/api/users/';
 
+  private cachedUsers$: Observable<any> | null = null;
+  private cachedUserDetails: { [key: number]: Observable<any> } = {};
+
   constructor(private _httpClient: HttpClient) {}
 
   getAllUsers(): Observable<any> {
-    return this._httpClient.get(this.apiBaseLink);
+    if (!this.cachedUsers$) {
+      this.cachedUsers$ = this._httpClient.get(this.apiBaseLink).pipe(
+        shareReplay(1),
+
+        tap(() => console.log('Fetched from API'))
+      );
+    } else {
+      console.log('returned chached data');
+    }
+    return this.cachedUsers$;
   }
+
   getSpecificUser(id: number): Observable<any> {
-    return this._httpClient.get(`https://reqres.in/api/users/${id}`);
+    if (!this.cachedUserDetails[id]) {
+      this.cachedUserDetails[id] = this._httpClient
+        .get(`${this.userDetailsBaseUrl}${id}`)
+        .pipe(
+          shareReplay(1),
+          tap(() => console.log(`Fetched user ${id} from API`))
+        );
+    } else {
+      console.log(`Returning cached data for user ${id}`);
+    }
+
+    return this.cachedUserDetails[id];
   }
 }
